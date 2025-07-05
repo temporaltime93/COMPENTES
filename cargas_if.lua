14f223b4ae91f5a7ab338bb72db245ff9b631f67
temporaltime93 <temporaltime93@gmail.com>
2025-07-05 14:50:52 -0500

    Delete cargas_if.lua

--- cargas_if.lua
+++
@@ -1,5 +0,0 @@
-local varible = _G.variable
-
-if varible === "tablero" then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/temporaltime93/COMPENTES/refs/heads/main/caja.lua"))()
-end
