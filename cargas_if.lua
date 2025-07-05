704561813173d2e0f1c6d06bb7923f8ac4034ad6
temporaltime93 <temporaltime93@gmail.com>
2025-07-05 14:51:15 -0500

    Create cargas_if.lua

---
+++ cargas_if.lua
@@ -0,0 +1,5 @@
+local varible = _G.variable
+
+if varible === "tablero" then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/temporaltime93/COMPENTES/refs/heads/main/caja.lua"))()
+end
